f314684ae45159d4872a058dfe6dccfedb15756d
summerpro <974741468@qq.com>
2021-09-14 11:08:43 +0800

    remove log

--- src/rarity/contracts/MultipleRarity.sol
+++ src/rarity/contracts/MultipleRarity.sol
@@ -57,8 +57,6 @@ interface rarity_skills {
 }
 
 contract MultipleRarity {
-    event Test1(address indexed user);
-    event Test2(address indexed user);
     rarity constant _rarity = rarity(0xce761D788DF608BD21bdd59d6f4B54b2e27F25Bb);
 
     rarity_gold constant _rarity_gold = rarity_gold(0x2069B76Afe6b734Fb65D1d099E7ec64ee9CC76B2);
